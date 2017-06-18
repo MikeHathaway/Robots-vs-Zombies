84ef0fcadfc1454a0977759b072fa00fb5f6cbbb
Mike <mahathaway93@gmail.com>
2017-06-17 20:26:34 -0700

    added init feathers and global gitignore

--- game-server/server/index.js
+++ game-server/server/index.js
@@ -1,2 +1,18 @@
 const feathers = require('feathers')
-const socketio = require('feathers-socketio');
+const bodyParser = require('body-parser')
+const path = require('path')
+const socketio = require('feathers-socketio')
+const postgresService = require('feathers-postgres')
+
+const app = feathers()
+  // Enable REST services
+  .configure(rest())
+  .configure(socketio());
+
+  // Turn on JSON parser for REST services
+  .use(bodyParser.json())
+  // Turn on URL-encoded parser for REST services
+  .use(bodyParser.urlencoded({ extended: true }));
+  .configure(services)
+
+function services(){}
